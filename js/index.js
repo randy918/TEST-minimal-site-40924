c00b55855b41849aef6f1e0c8bc359f263f3ff7a
Randy Miller <randy918@gmail.com>
2024-05-14 18:53:26 -0500

    5-14-24-1853

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-let email = luigi@gmail.com
+let a = 6788; 
+let b = 1288; 
+
+for(let i = 0; i < 5; i++){
+
+a = a + 100;
+console.log(a);
+
+if (a > 6988) {
+
+    console.log("Too Great");
+    continue;111
+
+}
+
+}
+
